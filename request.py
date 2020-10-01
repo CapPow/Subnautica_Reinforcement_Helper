60e85ee7b187ea413bbdb4655baa0935773840c5
Caleb Powell <calebadampowell@gmail.com>
2020-09-30 19:42:18 -0700

    Add files via upload

--- request.py
+++ request.py
@@ -1,33 +1,52 @@
 import zmq
 import time
 import random
-import base64
-import numpy as np
-import io
-from PIL import Image
+
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:12346")
 
 TIMEOUT = 5000
-time.sleep(2)
-for i in range(100):
-        time.sleep(0.05)
-        socket.send_string("get_outputs")
-        poller = zmq.Poller()
-        poller.register(socket, zmq.POLLIN)
-        evt = dict(poller.poll(TIMEOUT))
-        if evt:
-                if evt.get(socket) == zmq.POLLIN:
-                        response = socket.recv_string()
-                        score, base64string = response.split("|")
+time.sleep(3)
 
-                        jpg_img = base64.b64decode(base64string)
-                        with open(f"{i}.jpg", "wb") as fh:
-                                fh.write(jpg_img)
-                        print(f"score was:{score}, fn:{i}.png")
-                        if i == 0:
-                                print(base64string)
-        else:
-                print("Connection failed")
-                break
+TestScore = True
+
+if TestScore:
+        for i in range(1000):
+                time.sleep(0.1)
+                socket.send_string("get_outputs")
+                poller = zmq.Poller()
+                poller.register(socket, zmq.POLLIN)
+                evt = dict(poller.poll(TIMEOUT))
+                if evt:
+                        if evt.get(socket) == zmq.POLLIN:
+                                response = socket.recv_string()
+                                #score, pos = response.split("|")
+                                print(response)
+
+                else:
+                        print("Connection failed, Attempting to wait")
+                        break
+
+else:
+        for i in range(10):
+                time.sleep(10)
+                x = str(float(random.randrange(0, 800)))
+                y = "10.00"
+                z = str(float(random.randrange(-800, 800)))
+                loc_string = f"warp|{x}, {y}, {z}"
+                print(loc_string)
+                socket.send_string(loc_string)
+                poller = zmq.Poller()
+                poller.register(socket, zmq.POLLIN)
+                evt = dict(poller.poll(TIMEOUT))
+                if evt:
+                        if evt.get(socket) == zmq.POLLIN:
+                                response = socket.recv_string()
+                                #score, pos = response.split("|")
+                                print(response)
+
+                else:
+                        print("Connection failed, Attempting to wait")
+                        break
+
